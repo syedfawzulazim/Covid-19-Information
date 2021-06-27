61bd85af3a9d7097e65c0d83ad36433526e8c074
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-06-27 23:17:08 +0200

    New Cases, deaths and recovered added

--- js/main.js
+++ js/main.js
@@ -28,14 +28,62 @@ async function getData(){
         t[0].innerText = data.cases;
         t[1].innerText = data.deaths;
         t[2].innerText = data.recovered;
+        t[3].innerText = Math.round( data.casesPerWeek );
        
     }
     else{
-        alert("Please Wait For API to Response")
+        alert("Please Wait For API to Response");
     }
    
 
 }
 
-getData();
+function getDays(){
+    let days;
 
+    fetch(apiUrl+'/history/cases/7')
+    .then(res => {
+        if(res.ok){
+            return res.json();
+        }else{
+            console.log("error");
+        }
+    })
+    .then(data =>{
+        days = data.data;
+        return fetch(apiUrl+'/history/recovered/10');
+    })
+    .then(res => {
+        if(res.ok){
+            return res.json();
+        }else{
+            console.log("error");
+        }
+    })
+    .then(data => {
+        const recovered = data.data;
+
+        const tbody = document.querySelector('.tbody');
+
+      
+ //       console.log((days[0].date.slice(0, 10)))
+
+        for (let i = 0; i < days.length; i++) {
+            const tr = `<tr>
+                    <td>${days[i].date.slice(0, 10)} </td>
+                    <td>${days[i].cases} </td>
+                    <td>${recovered[i].recovered} </td>
+                </tr>`;
+             tbody.innerHTML += tr;
+}
+
+    }).catch( error => {
+        alert("please Wait...")
+    })
+}
+
+
+
+
+getData();
+getDays();
